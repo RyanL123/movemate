0e2a55a45223ecca84a489fac9e04831bc4d3468
Ryan Li <ryan.lichangrui@gmail.com>
2022-02-23 16:30:28 -0500

    add landing page image, still need to fix margin on form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { Box } from "@mui/material";
+import { Box, Paper, Grid, Button } from "@mui/material";
+import { ArrowForward } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DateTimePicker from "@mui/lab/DateTimePicker";
-import React from "react";
+import mountain from "./images/mountain.jpeg";
 
 const App = () => {
     const [date, setDate] = useState(null);
@@ -31,49 +32,102 @@ const App = () => {
         setCost(totalCost);
     }, [date, duration]);
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            paddingY="10vh"
-            paddingX="10vw"
-        >
-            <Typography variant="h1" textAlign="center">
-                Book your next escape
-            </Typography>
-            <Typography variant="h3" textAlign="center">
-                for just ${cost}
-            </Typography>
-            <Box marginY="5rem">
-                <TextField
-                    id="outlined-basic"
-                    label="Number of Hours"
-                    value={duration}
-                    onChange={(newDuration) => {
-                        // max 9999 hours ~ 416 days
-                        // using Math.min forces 0 in the input even when input is empty, not good
-                        if (newDuration.target.value > 9999) {
-                            setDuration(9999);
-                        } else {
-                            setDuration(newDuration.target.value);
-                        }
-                    }}
-                    variant="outlined"
-                    type="number"
-                />
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <DateTimePicker
-                        renderInput={(props) => <TextField {...props} />}
-                        label="Date and Time"
-                        value={date}
-                        onChange={(newDate) => {
-                            setDate(newDate);
+        <Grid container height="100vh">
+            <Grid
+                item
+                xs={6}
+                style={{
+                    backgroundImage: `url(${mountain})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center",
+                    padding: "5vw 10vh",
+                }}
+            ></Grid>
+            <Grid item xs={6} style={{ padding: "5vw 10vh" }}>
+                <Box
+                    height="100%"
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="space-evenly"
+                >
+                    <Typography
+                        variant="h2"
+                        textAlign="left"
+                        fontWeight="700"
+                        lineHeight="4rem"
+                    >
+                        Experience nature <br></br>like you've never before
+                    </Typography>
+                    <Paper
+                        sx={{
+                            paddingX: "3rem",
+                            paddingY: "2rem",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            borderRadius: "28px",
                         }}
-                    />
-                </LocalizationProvider>
-            </Box>
-        </Box>
+                        elevation={8}
+                    >
+                        <Typography
+                            variant="h5"
+                            textAlign="left"
+                            fontWeight="700"
+                        >
+                            Book your escape today
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            textAlign="left"
+                            fontWeight="400"
+                            color="text.secondary"
+                            marginBottom="1rem"
+                            lineHeight="1.5rem"
+                        >
+                            You're just a few steps away from paradise on earth
+                        </Typography>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DateTimePicker
+                                // sx={{ height: "100px" }}
+                                renderInput={(props) => (
+                                    <TextField color="secondary" {...props} />
+                                )}
+                                label="Date"
+                                value={date}
+                                onChange={(newDate) => {
+                                    setDate(newDate);
+                                }}
+                            />
+                        </LocalizationProvider>
+                        <TextField
+                            id="outlined-basic"
+                            label="Hours"
+                            value={duration}
+                            color="secondary"
+                            onChange={(newDuration) => {
+                                // max 9999 hours ~ 416 days
+                                // using Math.min forces 0 in the input even when input is empty, not good
+                                if (newDuration.target.value > 9999) {
+                                    setDuration(9999);
+                                } else {
+                                    setDuration(newDuration.target.value);
+                                }
+                            }}
+                            variant="outlined"
+                            type="number"
+                        />
+                        <Button
+                            variant="outlined"
+                            endIcon={<ArrowForward />}
+                            sx={{ height: "50px" }}
+                            color="secondary"
+                        >
+                            Book now for only ${cost}
+                        </Button>
+                    </Paper>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
